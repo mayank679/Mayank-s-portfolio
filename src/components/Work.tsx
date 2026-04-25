33921dd56c34ec350258d22807be78a54d3755e4
mayank679 <mayankraj6799@gmail.com>
2026-04-26 01:40:06 +0530

    Fix build error by registering ScrollTrigger and verify production build

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -4,7 +4,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const projects = [
   {
